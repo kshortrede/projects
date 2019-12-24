30375b751123c28419dbb9f322f85cbb3778bee6
kshortrede <42702544+kshortrede@users.noreply.github.com>
2019-12-24 09:05:17 -0500

    Add files via upload

---
+++ UnityProject/scripts/SoundEffects.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffects : MonoBehaviour
+{
+    public AudioClip[] footsteps;
+    public AudioClip[] death;
+
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Step()
+    {
+        audioSource.PlayOneShot(footsteps[0]);
+    }
+    private void Death()
+    {
+        audioSource.PlayOneShot(death[0]);
+    }
+}
